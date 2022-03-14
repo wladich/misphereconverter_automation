246b5ed4e09e14906339cca6e8388cfea25a5bc4
Sergej Orlov <wladimirych@gmail.com>
2022-03-14 09:22:26 +0100

    pass ssettings to andorid app via command line instead of xml

--- stitch.py
+++ stitch.py
@@ -59,13 +59,17 @@ class MSCCleint:
     def copy_file_from_vm(self, filename, dest_path):
         self.call_adb('pull', filename, dest_path)
 
-    def start_msc(self, image_filename, yaw_pitch_roll):
+    def start_msc(self, image_filename, yaw_pitch_roll, jpeg_quality=95, depurple=True, png=False, adaptive=3):
         retries = 10
         while retries:
             self.call_adb('shell', 'am force-stop %s' % package_name)
             command = 'am start -a STITCH_AUTOMATED --eu android.intent.extra.STREAM file://%s%s' % (vm_src_dir, image_filename)
             if yaw_pitch_roll is not None:
                 command += ' --ez ignore_exif true --ef yaw %.2f --ef pitch %.2f --ef roll %.2f' % tuple(yaw_pitch_roll)
+            command += ' --ei jpeg_q %s' % jpeg_quality
+            command += ' --ez depurple %s' % ('true' if depurple else 'false')
+            command += ' --ez lossless %s' % ('true' if png else 'false')
+            command += ' --ei adaptive %s' % adaptive
             command += ' %s/.IntentActivity' % package_name
             self.call_adb('shell', command)
             time.sleep(1)
@@ -87,26 +91,10 @@ class MSCCleint:
                 return False
         raise Exception('Unexpected result from ps | grep: code="%s", stdout="%s", stderr="%s"' % (p.returncode, stdout, stderr))
 
-    def write_settings(self, jpeg_quality=95, depurple=True, png=False, adaptive=3):
-        xml = '''<?xml version='1.0' encoding='utf-8' standalone='yes' ?>
-            <map>
-                <int name="adaptive" value="{adaptive}" />
-                <boolean name="depurple" value="{depurple}" />
-                <boolean name="tiff" value="{png}" />
-                <int name="jpg_q" value="{quality}" />
-            </map>
-        '''.format(quality=jpeg_quality, adaptive=adaptive,
-               depurple='true' if depurple else 'false', png='true' if png else 'false')
-        with tempfile.NamedTemporaryFile(mode='w') as config_file:
-            config_file.write(xml)
-            config_file.flush()
-            self.copy_file_to_vm(config_file.name, settings_file)
-
 
 def process_image(src_filename, dest_filename, png=False, calibration_filename=None, pose=None,
                   jpeg_quality=95, depurple=True, adaptive=3, adb_exec='adb'):
     client = MSCCleint(adb_exec)
-    client.write_settings(jpeg_quality, depurple, png, adaptive)
     client.ensure_empty_vm_dir(vm_src_dir)
     client.ensure_empty_vm_dir(vm_dest_dir)
     if calibration_filename:
@@ -114,7 +102,7 @@ def process_image(src_filename, dest_filename, png=False, calibration_filename=N
     client.copy_file_to_vm(src_filename, vm_src_dir)
     retries = 60
     ready_files = []
-    client.start_msc(os.path.basename(src_filename), pose)
+    client.start_msc(os.path.basename(src_filename), pose, jpeg_quality, depurple, png, adaptive)
     extension = '.png' if png else '.jpg'
     while retries:
         ready_files = [fn for fn in client.list_vm_dir(vm_dest_dir) if fn.lower().endswith(extension)]
